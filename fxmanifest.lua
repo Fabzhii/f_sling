a4ed670f266e8dbda1d57f57bbe2c3dd68921ad0
Fabzhii <131431631+Fabzhii@users.noreply.github.com>
2025-02-02 19:04:22 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'fabzhii'
+description 'F-Sling by fabzhii'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+shared_script '@ox_lib/init.lua'
+
+client_scripts {
+    "config.lua",
+    "client.lua",
+}
